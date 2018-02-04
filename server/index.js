f2d5786aa28a72be348eedcbefb20e15fce614c8
dhrjsoni01 <dhrjsoni01@gmail.com>
2018-02-04 11:20:09 +0530

    created user register function added address and location in user schema

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const router = express.Router();
-const port   = process.env.PORT || 3000 ;
+const express       = require('express');
+const app           = express();
+const bodyParser    = require('body-parser');
+const logger        = require('morgan');
+const router        = express.Router();
+const port          = process.env.PORT || 3000 ;
 
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
+
 app.get("/",function (req,res){
-    res.send("hello world!")
-} )
+    res.send("Welcome to our new app ")
+})
 
 // require('./routes')(router);
 // app.use('/api/v1', router);
