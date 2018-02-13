5ce55648b8ea4bbc0837b0d14ecbb71bb0e18f5d
dhrjsoni01 <dhrjsoni01@gmail.com>
2018-02-13 23:32:51 +0530

    remove andorid to make a separate project due to uncomfortabl to manage files

--- test.js
+++
@@ -1,9 +0,0 @@
-const name = "dheeraj kumar soni"
-splitname = name.split(' ')
-console.log(splitname[0]);
-console.log(splitname[1]);
-console.log(splitname[2]);
-console.log(splitname.length);
-
-
-
